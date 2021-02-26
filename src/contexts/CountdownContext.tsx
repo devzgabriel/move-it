f88ff0f6223ae377869a92d031c66a139e9c2aab
Gabriel Silva <devzgabriel@gmail.com>
2021-02-26 12:19:32 -0300

    prod time countdown

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -27,7 +27,7 @@ let countdownTimeOut: NodeJS.Timeout;
 export function CountdownProvider({ children }: CowntdownProviderProps) {
   const { startNewChallenge } = useContext(ChallengesContext);
 
-  const [time, setTime] = useState(0.1 * 60);
+  const [time, setTime] = useState(25 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
@@ -42,7 +42,7 @@ export function CountdownProvider({ children }: CowntdownProviderProps) {
     clearTimeout(countdownTimeOut);
     setIsActive(false);
     setHasFinished(false);
-    setTime(0.1 * 60);
+    setTime(25 * 60);
   }
 
   useEffect(() => {
